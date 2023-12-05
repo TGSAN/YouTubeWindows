2ffc48788746a61975a6c98fd18850f26e308ebb
TGSAN <h@iloli.tv>
2023-12-06 04:41:23 +0800

    DARK MAGIC!

--- YouTubeWindows/MainForm.cs
+++ YouTubeWindows/MainForm.cs
@@ -21,6 +21,9 @@ namespace YouTubeWindows
 
     public partial class MainForm : Form
     {
+        [DllImport("dwmapi.dll")]
+        private static extern int DwmSetWindowAttribute(IntPtr hwnd, int attr, ref int attrValue, int attrSize);
+        
         public string webview2StartupArgs = "";
         WebView2RuntimeInfo? webview2RuntimeInfo = null;
         private CoreWebView2Environment coreWebView2Environment;
@@ -59,6 +62,31 @@ namespace YouTubeWindows
                     WindowState = FormWindowState.Normal;
                 }
             }
+        }
+
+        private const int DWMWA_USE_IMMERSIVE_DARK_MODE_BEFORE_20H1 = 19;
+        private const int DWMWA_USE_IMMERSIVE_DARK_MODE = 20;
+
+        private static bool UseImmersiveDarkMode(IntPtr handle, bool enabled)
+        {
+            if (IsWindows10OrGreater(17763))
+            {
+                var attribute = DWMWA_USE_IMMERSIVE_DARK_MODE_BEFORE_20H1;
+                if (IsWindows10OrGreater(18985))
+                {
+                    attribute = DWMWA_USE_IMMERSIVE_DARK_MODE;
+                }
+
+                int useImmersiveDarkMode = enabled ? 1 : 0;
+                return DwmSetWindowAttribute(handle, (int)attribute, ref useImmersiveDarkMode, sizeof(int)) == 0;
+            }
+
+            return false;
+        }
+
+        private static bool IsWindows10OrGreater(int build = -1)
+        {
+            return Environment.OSVersion.Version.Major >= 10 && Environment.OSVersion.Version.Build >= build;
         }
 
         private WebView2RuntimeInfo? ReadRuntime(string path)
@@ -136,6 +164,8 @@ namespace YouTubeWindows
             webview2StartupArgs = webview2StartupArgsBuilder.ToString();
 
             InitializeComponent();
+            
+            UseImmersiveDarkMode(this.Handle, true);
 
             this.Icon = Resource.icon;
 
